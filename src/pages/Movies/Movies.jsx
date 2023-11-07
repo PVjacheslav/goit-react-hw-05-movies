b93a6aeeffb5e367b023eeb2273a2d1053b54516
PVjacheslav <128468517+PVjacheslav@users.noreply.github.com>
2023-11-07 16:15:50 +0200

    move mistakes

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -4,12 +4,13 @@ import { fetchSearchKeyword } from "helpers/TmdbApi";
 import MoviesList from "pages/MoviesList/MoviesList";
 import { useEffect, useState } from "react"
 import { useSearchParams } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Movies = () => {
     const [searchParams, setSearchParams] = useSearchParams()
     const [searchFilms, setSearchFilms] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [noMoviesText, setNoMoviesText] = useState(false);
+    const [error, setError] = useState(false);
 
     const getQueryUrl = searchParams.get('query') ?? '';
 
@@ -22,32 +23,49 @@ const Movies = () => {
         if (getQueryUrl === '') {
             return;
         }
-    })   
-    
-    const searchMovies = queryMovie => {
-        setLoading(true);
 
-        fetchSearchKeyword(queryMovie)
-            .then(searchResults => {
-                setSearchFilms(searchResults);
-                setNoMoviesText(searchResults.length === 0);
-            })
-            .catch(error => {
-                console.log(error);
-            })
-            .finally(() => {
+        async function searchMovie() {
+            setSearchFilms([]);
+            setLoading(true);
+            try {
+                const response = await fetchSearchKeyword(getQueryUrl);
+                if (response.results.length === 0) {
+                    return toast.error('Sorry, there are no movies. Try again');
+                }
+                setSearchFilms(response.results);
+            } catch {
+                setError(true)
+            } finally {
                 setLoading(false)
-            });
-    };
+            }
+        }
+        searchMovie()
+    }, [getQueryUrl])   
+    
+    // const searchMovies = queryMovie => {
+    //     setLoading(true);
+
+    //     fetchSearchKeyword(queryMovie)
+    //         .then(searchResults => {
+    //             setSearchFilms(searchResults);
+    //             setNoMoviesText(searchResults.length === 0);
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         })
+    //         .finally(() => {
+    //             setLoading(false)
+    //         });
+    // };
 
     return (
         <main>
-            <Form searchMovies={searchMovies} />
+            <Form searchMovies={handleSubmit} />
             {loading && <Loader />}
-            {noMoviesText && (
+            {error && (
                 <p>No movie was found for your request. Please try again.</p>
             )}
-            {searchFilms && <MoviesList films={searchFilms} />}
+            {searchFilms.length !==0 && <MoviesList films={searchFilms} />}
         </main>
     );
 };
